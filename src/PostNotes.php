5cff7387071aca93a2686d059a61fbf33d9d9b2a
Vlad Olaru <vlad@pixelgrade.com>
2021-08-13 14:06:21 +0300

    Version bump to 0.5.1

--- src/PostNotes.php
+++ src/PostNotes.php
@@ -83,7 +83,7 @@ class PostNotes {
 
 		# Define version constant
 		if ( ! defined( __NAMESPACE__ . '\VERSION' ) ) {
-			define( __NAMESPACE__ . '\VERSION', '0.1.0' );
+			define( __NAMESPACE__ . '\VERSION', '0.5.1' );
 		}
 
 		# Define root directory
